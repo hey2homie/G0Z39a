8ede5ebd383698c13c48639ea434d32bd54d73f0
hey2homie <photocard4@gmail.com>
2021-05-27 03:25:44 +0200

    Still trying

--- code/actual code/app.py
+++ code/actual code/app.py
@@ -1,8 +1,9 @@
 import os
 
 import pandas as pd
-
 import json
+
+import flask
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
@@ -42,8 +43,8 @@ for i in countries["features"]:
     i["id"] = (i["properties"]["name"])
 
 # App
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-server = app.server
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], server=server)
 
 app.layout = html.Div(
     className="Main",
