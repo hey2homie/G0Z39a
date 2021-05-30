32efdc7b4a6bb720598d97599dd6c6dd06812bfc
TAOFENG BU <51697703+endvia@users.noreply.github.com>
2021-05-30 23:51:18 +0200

    Update preprocessing.py
    updated: 
    1. changed back the normalization method, in which I used the pandas Min-Max 
    
    2. the code might look repeated for data 2016 and 2020, but the original version is mean to deal with the data of one year separaterly and in the final step merge them together. I try the last version, but it did not work well and the result was changed. Since what Jingtong did depends on the result from the original version so I think that it would be better to keep the processed data file unchanged for the consistency of our data and the result.

--- code/actual_code/preprocessing.py
+++ code/actual_code/preprocessing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 from sklearn.impute import KNNImputer
-from sklearn.preprocessing import normalize
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Produces different data than /raw_code/preprocessing cause. Since nobody responsible fixed it, the results should be
@@ -13,31 +13,57 @@ from sklearn.preprocessing import normalize
 class Preprocessing:
 
     def __init__(self, dataframe2016, dataframe2020, index2016, index2020):
-        self.df2016 = pd.merge(pd.read_csv(dataframe2016), pd.read_csv(index2016),
-                               on="country", how="inner")
-        self.df2020 = pd.merge(pd.read_excel(dataframe2020), pd.read_csv(index2020),
-                               on="country", how="inner")
-        self.df = self.df2016.append(self.df2020)
-        self.df = pd.read_csv(dataframe2016)
+        self.df2016 = pd.read_csv(dataframe2016)
+        self.df2020 = pd.read_csv(dataframe2020)
+        self.df_coordinate = pd.read_csv("./data/raw data/coordinates.csv")
+
+        self.df2016 = pd.merge(self.df2016, self.df_coordinate, how='left', on='country')
+        self.df2020 = pd.merge(self.df2020, self.df_coordinate, how='left', on='country')
+
+        self.df_index2016 = pd.read_csv(index2016)
+        self.df_index2020 = pd.read_csv(index2020)
+
         self.output_name = None
-        self.countries = None
 
     def replace(self):
-        self.df = self.df[self.df.isna().sum(axis=1) < 19]
-        self.df.replace({'-': np.nan, '0': np.nan, '>99': 99})
+        self.df2016 = self.df2016[self.df2016.isna().sum(axis=1) < 19]
+        self.df2016.replace({'0': np.nan})
+
+        self.df2020 = self.df2020[self.df2020.isna().sum(axis=1) < 19]
+        self.df2020.replace({'0': np.nan})
 
     def drop(self):
-        threshold = self.df.shape[0] * 0.55
-        self.df.dropna(axis="columns", thresh=threshold, inplace=True)
-        self.df.drop("Unnamed: 0", axis=1, inplace=True)
+         threshold2016 = self.df2016.shape[0] * 0.55
+         self.df2016.dropna(axis="columns", thresh=threshold2016, inplace=True)
+         self.df2016.drop("Unnamed: 0", axis=1, inplace=True)
+
+         threshold2020 = self.df2020.shape[0] * 0.55
+         self.df2020.dropna(axis="columns", thresh=threshold2020, inplace=True)
+         self.df2020.drop("Unnamed: 0", axis=1, inplace=True)
 
     def fill_na(self):
         imputer = KNNImputer(n_neighbors=8)
-        length = len(self.df.columns)
-        self.df.iloc[:, 2:length] = pd.DataFrame(imputer.fit_transform(self.df.iloc[:, 2:length]))
+        length = len(self.df2016.columns)
+        self.df2016.iloc[:, 2:length] = imputer.fit_transform(self.df2016.iloc[:, 2:length])
+
+        length = len(self.df2020.columns)
+        self.df2020.iloc[:, 2:length] = imputer.fit_transform(self.df2020.iloc[:, 2:length])
 
     def normalization(self):
-        self.df.iloc[:, 1:len(self.df.columns)] = normalize(self.df.iloc[:, 1:len(self.df.columns)])
+        length2016 = len(self.df2016.columns)
+        self.df2016.iloc[:,1:length2016] = self.df2016.iloc[:,1:length2016].apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
+
+        length2020 = len(self.df2020.columns)
+        self.df2020.iloc[:, 1:length2020] = self.df2020.iloc[:, 1:length2020].apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
+
+    def merge_index(self):
+         self.df2016 = pd.merge(self.df2016,self.df_index2016, on="country", how="inner")
+         self.df2020 = pd.merge(self.df2020, self.df_index2020, on="country", how="inner")
+         self.df = self.df2020.append(self.df2016)
+
+         self.df.drop("country", axis=1, inplace=True)
+         self.df.drop("Flood occurrence (WRI) (-)", axis=1, inplace=True)
+
 
     def save(self, output_name, norm=True, write=True):
         self.replace()
@@ -45,9 +71,19 @@ class Preprocessing:
         self.fill_na()
         if norm:
             self.normalization()
+
+        self.merge_index()
         if write:
-            self.output_name = "../../data/final data/" + output_name + ".csv"
+            self.output_name = "./data/final data/" + output_name + ".csv"
             self.df.to_csv(self.output_name)
+
 
     def get_dataframe(self):
         return pd.read_csv(self.output_name)
+
+
+
+
+#data2016 = Preprocessing("./data/raw data/2016raw.csv","./data/raw data/2020raw.csv","./data/raw data/2016APindex.csv","./data/raw data/2020APindex.csv")
+#data2016.save("newData", norm=True)
+
